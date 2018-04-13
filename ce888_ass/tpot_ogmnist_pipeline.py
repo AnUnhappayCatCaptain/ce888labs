74f3b1313d330285c02999f40307e33178cdf9ad
AnUnhappayCatCaptain <528617028@qq.com>
2018-04-13 17:08:03 +0100

    update

---
+++ ce888_ass/tpot_ogmnist_pipeline.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from sklearn.kernel_approximation import Nystroem
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import make_pipeline
+
+# NOTE: Make sure that the class is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1).values
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'].values, random_state=42)
+
+# Score on the training set was:1.0
+exported_pipeline = make_pipeline(
+    Nystroem(gamma=0.45, kernel="cosine", n_components=1),
+    GaussianNB()
+)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
