19e8c1e6e940eb626dc5a4c0c5ea75852b1e6180
AnUnhappayCatCaptain <528617028@qq.com>
2018-04-21 08:37:36 +0100

    ce888labs

--- ce888_ass/tpot_ogmnist_pipeline.py
+++ ce888_ass/tpot_ogmnist_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.kernel_approximation import Nystroem
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import make_pipeline
+from tpot.builtins import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +11,10 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:1.0
+# Score on the training set was:0.989166666667
 exported_pipeline = make_pipeline(
-    Nystroem(gamma=0.45, kernel="cosine", n_components=1),
-    GaussianNB()
+    ZeroCount(),
+    ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.3, min_samples_leaf=10, min_samples_split=18, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
