cb71b7e0da02381d8b570c416bf744d56e1a3258
AnUnhappayCatCaptain <528617028@qq.com>
2018-04-26 04:31:41 +0100

    ce888labs

--- ce888_ass/tpot_ogmnist_pipeline.py
+++ ce888_ass/tpot_ogmnist_pipeline.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
-from tpot.builtins import ZeroCount
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,11 +9,8 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.989166666667
-exported_pipeline = make_pipeline(
-    ZeroCount(),
-    ExtraTreesClassifier(bootstrap=False, criterion="gini", max_features=0.3, min_samples_leaf=10, min_samples_split=18, n_estimators=100)
-)
+# Score on the training set was:0.932915681058
+exported_pipeline = RandomForestClassifier(bootstrap=False, criterion="gini", max_features=0.7, min_samples_leaf=2, min_samples_split=4, n_estimators=100)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
